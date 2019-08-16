3fb8747cc712f4f0e2b3285cc8599a7a72959c38
Brad Dunn <brad@autobooks.co>
2019-08-16 17:08:35 -0400

    Introduce MaterialUI and cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { Typography, Container } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App" data-testid="base-app">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Container data-testid="base-app">
+      <header>
+        <Typography variant="h1">DM Screen</Typography>
       </header>
-    </div>
+      <main>
+        <Typography variant="body1">
+          Brad's personal DM screen for all the wonderful things.
+        </Typography>
+      </main>
+    </Container>
   );
 }
 
