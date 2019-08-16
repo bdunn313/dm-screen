f516b94421505117b39f2b92d74dcf034f48a558
Brad Dunn <brad@autobooks.co>
2019-08-16 16:38:04 -0400

    Basic setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from "./App";
+
+it("renders without crashing", () => {
+  const { getByTestId } = render(<App />);
+  getByTestId("base-app");
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-const App: React.FC = () => {
+function App() {
   return (
-    <div className="App">
+    <div className="App" data-testid="base-app">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
