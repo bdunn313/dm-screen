8da8c2fb7deeb825a9f096a33760a5d16a4f4e84
Brad Dunn <brad@braddunn.com>
2019-09-15 18:40:55 -0400

    getting closer to a solution here - not 100% ready yet

--- functions/get-character-json/get-character-json.js
+++ functions/get-character-json/get-character-json.js
@@ -1,10 +1,30 @@
+const chrome = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+
+async function getContent(url) {
+  const browser = await puppeteer.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  });
+
+  const page = await browser.newPage();
+  await page.goto(url);
+  // const content = await page.content();
+  const element = await page.$("pre");
+  const text = await (await element.getProperty('textContent')).jsonValue();
+  await browser.close();
+  return text;
+}
+
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || "World";
+    const subject = event.queryStringParameters.url || "https://google.com/";
+    const result = await getContent(subject)
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` })
+      body: JSON.stringify(result)
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
